f2adc8a48d35c41b96d6363604e66bebc0b8af1b
Abhijatya Gupta <48697844+abhijatyagupta@users.noreply.github.com>
2021-02-18 00:19:30 +0530

    Add comments to Kruskal's algorithm
    Added comments to kruskal's algorithm for better understanding.

--- Geeks for Geeks/MInimum Spanning Tree/KruskalsAlgorithm.java
+++ Geeks for Geeks/MInimum Spanning Tree/KruskalsAlgorithm.java
@@ -4,40 +4,40 @@ import java.lang.*;
 
 class Solution {
     static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) {
-        int[] parent = new int[V];
+        int[] parent = new int[V];  // will keep track of parent of each node
 
-        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        PriorityQueue<Edge> queue = new PriorityQueue<>();  // will always provide edge with minimum weight
 
         for(int i = 0; i < V; i++) {
             for(int j = 0; j < adj.get(i).size(); j++) {
                 int destination = adj.get(i).get(j).get(0);
                 int weight = adj.get(i).get(j).get(1);
                 Edge edge = new Edge(i, destination, weight);
-                if (!queue.contains(edge)) queue.offer(edge);
+                if (!queue.contains(edge)) queue.offer(edge); // makes sure no duplicate edge from adjacency list is added (for e.g. same edge from u to v and v to u)
             }
-            parent[i] = -1;
+            parent[i] = -1; // no node will have a parent initially
         }
 
-        int sum = 0;
+        int sum = 0;    // will hold sum of edges that are part of MST (the required answer, basically)
         while(!queue.isEmpty()) {
-            Edge edge = queue.poll();
-            if (cycleExists(parent, edge)) continue;
-            sum += edge.weight;
+            Edge edge = queue.poll();   // removes the edge with minimum weight from priority queue
+            if (cycleExists(parent, edge)) continue;    // don't add weight of edge to sum if it forms a cycle
+            sum += edge.weight; // add weight of the edge to sum, now that it does not form a cycle
         }
         return sum;
     }
 
     static boolean cycleExists(int[] parent, Edge edge) {
-        int sourceParent = findParent(parent, edge.source);
-        int destinationParent = findParent(parent, edge.destination);
-        if (sourceParent == destinationParent) return true;
-        parent[destinationParent] = sourceParent;
-        return false;
-    }
+        int sourceParent = findParent(parent, edge.source); // find the parent of source node
+        int destinationParent = findParent(parent, edge.destination);   // find the parent of destination node
+        if (sourceParent == destinationParent) return true; // if both parents are same, return that cycle exists
+        parent[destinationParent] = sourceParent;   // if parents are different, make their parent same, hence considering their common edge in MST
+        return false; // returns that no cycle will be formed on making this edge a part of MST
+    }   // https://youtu.be/wU6udHRIkcc <-- see this video to understand union-find and disjoint sets
 
     static int findParent(int[] parent, int node) {
-        if (parent[node] == -1) return node;
-        return findParent(parent, parent[node]);
+        if (parent[node] == -1) return node; // if the parent of node == -1, the node itself is the parent, so return it
+        return findParent(parent, parent[node]); // else find grandparent of node
     }
 }
 
@@ -55,12 +55,12 @@ class Edge implements Comparable<Edge> {
     }
 
     @Override
-    public int compareTo(Edge o) {
+    public int compareTo(Edge o) {      //required for priority queue
         return this.weight - o.weight;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) {   //required for queue.contains()
         if (o instanceof Edge) {
             return this.source == ((Edge)o).source && this.destination == ((Edge)o).destination;
         }

e2a49ce40e7945bd0d816bd261e0797f2b4b3def
Abhijatya Gupta <48697844+abhijatyagupta@users.noreply.github.com>
2021-07-09 18:03:08 +0530

    Change if statement indentation

--- LeetCode/Best Time to Buy and Sell Stock II/best-time-to-buy-and-sell-stock-2.swift
+++ LeetCode/Best Time to Buy and Sell Stock II/best-time-to-buy-and-sell-stock-2.swift
@@ -3,7 +3,9 @@ class Solution {
         if (prices.count == 1) { return 0 }
         var profit : Int = 0
         for i in 1..<prices.count {
-            if (prices[i] > prices[i-1]) { profit += prices[i] - prices[i-1] }
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1]
+            }
         }
         return profit
     }

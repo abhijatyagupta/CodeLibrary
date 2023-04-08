f3c862cf560a76fb84296296dbef7c355ce29c8a
Abhijatya Gupta <48697844+abhijatyagupta@users.noreply.github.com>
2023-04-08 21:59:27 +0530

    remove board variable

--- Geeks for Geeks/N-Queen Problem/n-queen.java
+++ Geeks for Geeks/N-Queen Problem/n-queen.java
@@ -6,35 +6,32 @@ class Solution {
     static ArrayList<ArrayList<Integer>> nQueen(int n) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>(); // list that will hold all the possible permutations
         for(int i = 0; i < n; i++) {    // traverse for each cell of the first row
-            boolean[][] board = new boolean[n][n];  // the chess board
             boolean[][] canPlaceQueen = new boolean[][] {
                 new boolean[n], // will tell whether a column is occupied by a queen. (Such an array is not required for rows since the board will be traversed row by row and it will be obvious that no queen already exist in the current row that is being traversed)
                 new boolean[2*n],   // will tell whether top-left diagonal is occupied
                 new boolean[2*n]    // will tell whether top-right diagonal is occupied
             };
             ArrayList<Integer> permutation = new ArrayList<>(); // will hold a particular permutation
-            nQueenHelper(board, canPlaceQueen, list, permutation, 0, i); // call to actual shit
+            nQueenHelper(n, canPlaceQueen, list, permutation, 0, i); // call to actual shit
         }
         return list;
     }
 
-    static void nQueenHelper(boolean[][] board, boolean[][] canPlaceQueen, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> permutation, int row, int col) {
-        if (canPlaceQueen[0][col] || canPlaceQueen[1][(board.length - 1) + col-row] || canPlaceQueen[2][col+row]) return;   // return if another queen can attack this cell
+    static void nQueenHelper(int boardLength, boolean[][] canPlaceQueen, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> permutation, int row, int col) {
+        if (canPlaceQueen[0][col] || canPlaceQueen[1][(boardLength - 1) + col-row] || canPlaceQueen[2][col+row]) return;   // return if another queen can attack this cell
         permutation.add(col+1); // add current cell to the permutation (+1 because solution required one-based indexing)
-        board[row][col] = true; // mark position on board as occupied
-        canPlaceQueen[0][col] = canPlaceQueen[1][(board.length - 1) + col-row] = canPlaceQueen[2][col+row] = true; // mark the column and diagonals as occupied
+        canPlaceQueen[0][col] = canPlaceQueen[1][(boardLength - 1) + col-row] = canPlaceQueen[2][col+row] = true; // mark the column and diagonals as occupied
         
-        for(int i = 0; i < board.length && ((row+1) < board.length); i++) { // find all possible safe permutations for the next queen in the next row
-            nQueenHelper(board, canPlaceQueen, list, permutation, row+1, i);
+        for(int i = 0; i < boardLength && ((row+1) < boardLength); i++) { // find all possible safe permutations for the next queen in the next row
+            nQueenHelper(boardLength, canPlaceQueen, list, permutation, row+1, i);
         }
         
-        if (permutation.size() == board.length) {   // if this is the final row/queen of the permutation
+        if (permutation.size() == boardLength) {   // if this is the final row/queen of the permutation
             list.add(new ArrayList<>(permutation)); // add the permutation to the list
         }
         
         permutation.remove(permutation.size() - 1); // backtrack to find other possible permutations
-        board[row][col] = false;
-        canPlaceQueen[0][col] = canPlaceQueen[1][(board.length - 1) + col-row] = canPlaceQueen[2][col+row] = false;
+        canPlaceQueen[0][col] = canPlaceQueen[1][(boardLength - 1) + col-row] = canPlaceQueen[2][col+row] = false;
     }
 }
 
